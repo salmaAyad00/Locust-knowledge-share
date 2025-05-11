d48820b22b7999258773591a27d73d19a5d02bfe
salmaAyad00 <salma.ayad@flextock.com>
2025-05-11 09:27:47 +0300

    Add README and initial implementation of HttpUsersTest for Locust load testing

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Locust-knowledge-share
-This repo to practice locust for first time
+# HttpUsersTest - Locust Load Test for User API
+
+This file contains a basic Locust load test to simulate HTTP requests to the ReqRes API for user-related actions.
+
+## Description
+
+`http_users.py` defines a Locust `HttpUser` class that performs two types of HTTP requests:
+
+1. **GET /users?page=2**: Fetches a list of users from page 2.
+2. **POST /users**: Creates a new user with specified name and job attributes.
+
+Each task will print the HTTP status code received in the response.
+
+### core example on login, list outbound orders
+
---
+++ http_users.py
@@ -0,0 +1,45 @@
+from locust import HttpUser, constant, task
+from requests import RequestException
+
+
+class HttpUsersTest(HttpUser):
+    wait_time=constant(1)
+    host= "https://reqres.in/api"
+    
+    @task
+    def get_users(self):
+        res=self.client.get("/users?page=2")
+        print(f"Response of get user: {res.status_code}")
+        
+    @task
+    def create_user(self):
+        res=self.client.post("/users", json={"name": "morpheus", "job": "leader"})
+        print(f"Response of create user: {res.status_code}")
+        
+        
+class HttpcoreUser(HttpUser):
+    wait_time=constant(1)
+    host= "https://core-stg-rkcv4cncjq-uc.a.run.app"
+    
+    @task
+    def login(self):
+        res=self.client.post("/auth/login/", 
+                             json={"email": "salma.ayad+password@flextock.com", "password": "Omar@1234"})
+        print(f"Response of login: {res.status_code}")
+    
+    @task
+    def get_orders(self):
+        try:
+            res=self.client.get("/orders/outbounds/?sort[]=-created_at&page=1&per_page=100", headers=
+                            {
+                    "Authorization":
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQ2OTQ3MDc0LCJpYXQiOjE3NDY5NDMxODgsImp0aSI6IjhmN2EyZTE1MTFhMjQ0ZmFhYzJiYzBiMzA0Y2IzYjNmIiwidXNlcl9pZCI6MTIxLCJlbWFpbCI6InNhbG1hLmF5YWQrcGFzc3dvcmRAZmxleHRvY2suY29tIiwibmFtZSI6InBhc3N3b3JkLWNoZWNrIiwic3RhZmYiOnRydWUsImlzX21hc3Rlcl91c2VyIjpmYWxzZSwiaXNfZW1haWxfdmVyaWZpZWQiOnRydWUsInBlcm1pc3Npb25zIjpbXX0.Sks4t1sWCEHlDMAdSy8kaMhcWnzulRnHk45EZ4m_mXg"
+                    })
+            print(f"Response of list orders: {res.status_code}")
+        except RequestException as e:
+            print(f"Request failed: {e}")    
+    
+    
+        
+        
+        
